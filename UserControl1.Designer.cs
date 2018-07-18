11d418c28d9762cda5dd682761d6e9ca7cc5e9c2
M.F.Aziz <azizsagi@yahoo.com>
2018-07-18 15:22:15 +0300

    Create DLL project in C#
    Create DLL project in C#

---
+++ UserControl1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace myDLL
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // text1
+            // 
+            this.text1.Location = new System.Drawing.Point(3, 3);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(163, 20);
+            this.text1.TabIndex = 0;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.text1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(171, 28);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox text1;
+    }
+}
